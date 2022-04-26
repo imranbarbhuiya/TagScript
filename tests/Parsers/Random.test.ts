56ebd492011bca13987d606a034c1d2cf666b093
parbez <imranbarbhuiya.fsd@gmail.com>
2022-04-27 00:16:37 +0530

    test: fix random test

--- tests/Parsers/Random.test.ts
+++ tests/Parsers/Random.test.ts
@@ -2,8 +2,8 @@ import { Interpreter, RandomParser } from '../../src';
 const ts = new Interpreter(new RandomParser());
 describe('RandomParser', () => {
 	test('Given a string in random parser THEN returns a random string from the payload', async () => {
-		expect((await ts.run('{random:rkn,priayansh,kashish} won the game')).body).toMatch(
-			/(rkn|priyansh|kashish) won the game/,
+		expect((await ts.run('{random:rkn,priyansh,kashish} won the game')).body).toMatch(
+			/(?:rkn|priyansh|kashish) won the game/,
 		);
 	});
 });
