811cf95e3a34b6b3b8aaff92398a4f7be272680a
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-04-30 19:13:15 +0530

    feat: add urldecode parser (#17)
    * feat: add urldecode parser
    
    * fix: typo

--- src/lib/Parsers/UrlEncoded.ts
+++ src/lib/Parsers/UrlEncoded.ts
@@ -2,12 +2,49 @@ import type { IParser } from '../interfaces';
 import type { Context } from '../Interpreter';
 import { BaseParser } from './Base';
 
+/**
+ * This tag will encode a given string into a properly formatted url
+ * with non-url compliant characters replaced. Using `+` as the parameter
+ * will replace spaces with `+` rather than `%20`.
+ *
+ * @alias encodeuri
+ *
+ * @usage
+ * ```yaml
+ * 		{urlencode:Hello World}
+ * ```
+ */
 export class UrlEncodeParser extends BaseParser implements IParser {
 	public constructor() {
 		super(['urlencode', 'encodeuri'], false, true);
 	}
 
 	public parse(ctx: Context) {
-		return encodeURI(ctx.tag.payload!);
+		return ctx.tag.parameter === '+'
+			? encodeURI(ctx.tag.payload!.replace(/ +/g, ctx.tag.parameter))
+			: encodeURI(ctx.tag.payload!);
+	}
+}
+
+/**
+ * This tag will decode a given url into a string
+ * with non-url compliant characters replaced.
+ * Using `+` as the parameter will replace `+` with space.
+ *
+ * @usage
+ * ```yaml
+ * 		{urldecode:Hello%20World}
+ * ```
+ *
+ */
+export class UrlDecodeParser extends BaseParser implements IParser {
+	public constructor() {
+		super(['urldecode'], false, true);
+	}
+
+	public parse(ctx: Context) {
+		return ctx.tag.parameter === '+'
+			? decodeURI(ctx.tag.payload!.replaceAll(ctx.tag.parameter, ' '))
+			: decodeURI(ctx.tag.payload!);
 	}
 }
--- tests/Parsers/UrlEncoded.test.ts
+++ tests/Parsers/UrlEncoded.test.ts
@@ -1,9 +1,27 @@
-import { Interpreter, Response, UrlEncodeParser } from '../../src';
-const ts = new Interpreter(new UrlEncodeParser());
+import { Interpreter, Response, UrlDecodeParser, UrlEncodeParser } from '../../src';
+
+const ts = new Interpreter(new UrlEncodeParser(), new UrlDecodeParser());
+
 describe('UrlEncodedParser', () => {
-	test('Given a string in UrlEncoded parser THEN returns the urlencoded string', async () => {
-		expect(await ts.run('{urlencode:This is Rkn}')).toStrictEqual(
-			new Response().setValues(encodeURI('This is Rkn'), '{urlencode:This is Rkn}'),
-		);
+	test('Given a string in UrlEncode parser THEN returns the urlencoded string', async () => {
+		const text = '{urlencode:This is Rkn}';
+		expect(await ts.run(text)).toStrictEqual(new Response().setValues('This%20is%20Rkn', text));
+	});
+
+	test('Given a string in UrlEncode parser with + param THEN returns the urlencoded string by replacing space with +', async () => {
+		const text = '{urlencode(+):This is Rkn}';
+		expect(await ts.run(text)).toStrictEqual(new Response().setValues('This+is+Rkn', text));
+	});
+});
+
+describe('UrlDecodedParser', () => {
+	test('Given a string in UrlDecode parser THEN returns the urldecoded string', async () => {
+		const text = '{urldecode:This%20is%20Rkn}';
+		expect(await ts.run(text)).toStrictEqual(new Response().setValues('This is Rkn', text));
+	});
+
+	test('Given a string in UrlDecode parser with + param THEN returns the urldecoded string by replacing + with space', async () => {
+		const text = '{urldecode(+):This+is+Rkn}';
+		expect(await ts.run(text)).toStrictEqual(new Response().setValues('This is Rkn', text));
 	});
 });
