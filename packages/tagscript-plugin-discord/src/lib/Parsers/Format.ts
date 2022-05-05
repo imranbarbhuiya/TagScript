f791cf908ba015fb595be64e0a4470069ab95459
parbez <imranbarbhuiya.fsd@gmail.com>
2022-05-05 22:21:44 +0530

    fix: remove other formats

--- packages/tagscript-plugin-discord/src/lib/Parsers/Format.ts
+++ packages/tagscript-plugin-discord/src/lib/Parsers/Format.ts
@@ -1,43 +1,5 @@
-import { BaseParser, escapeContent, type Context, type IParser } from 'tagscript';
+import { BaseParser, type Context, type IParser } from 'tagscript';
 
-/**
- * This tag formats a given string.
- *
- * @example
- * ```yaml
- *     {lower:Hello Parbez!}
- *     # hello parbez!
- *     {upper:Hello Parbez!}
- *     # HELLO PARBEZ!
- *     {capitalize:hello parbez!}
- *     # Hello parbez!
- *     {escape:Hello| Parbez!}
- *     # Hello\\| Parbez!
- * ```
- */
-export class StringFormatParser extends BaseParser implements IParser {
-	public constructor() {
-		super(['lower', 'upper', 'capitalize', 'escape'], false, true);
-	}
-
-	public parse(ctx: Context) {
-		const { declaration, payload } = ctx.tag;
-		switch (declaration) {
-			case 'lower':
-				return payload!.toLowerCase();
-			case 'upper':
-				return payload!.toUpperCase();
-			case 'capitalize':
-				return payload!.charAt(0).toUpperCase() + payload!.slice(1);
-			case 'escape':
-				return escapeContent(payload!);
-			default:
-				return payload!;
-		}
-	}
-}
-
-// This is a discord specific formatter
 export class DateFormatParser extends BaseParser implements IParser {
 	public constructor() {
 		super(['date', 'unix', 'currentTime'], false, true);
@@ -56,19 +18,5 @@ export class DateFormatParser extends BaseParser implements IParser {
 		if (payload.length > 10) payload = Math.floor(Number(payload) / 1000);
 
 		return `<t:${payload}:${parameter}>`;
-	}
-}
-
-export class OrdinalFormatParser extends BaseParser implements IParser {
-	public constructor() {
-		super(['ordinal', 'ord'], false, true);
-	}
-
-	public parse(ctx: Context) {
-		const { payload } = ctx.tag;
-		if (isNaN(Number(payload))) return payload;
-		const lastDigit = payload!.slice(-1);
-		const suffix = lastDigit === '1' ? 'st' : lastDigit === '2' ? 'nd' : lastDigit === '3' ? 'rd' : 'th';
-		return `${payload}${suffix}`;
 	}
 }
