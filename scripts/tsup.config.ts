c6a0bbc0892d648ba42f84810d1eaa1670db6b8d
parbez <imranbarbhuiya.fsd@gmail.com>
2022-08-21 18:21:30 +0530

    test: add c8 package

--- scripts/tsup.config.ts
+++ scripts/tsup.config.ts
@@ -1,24 +1,17 @@
 import { relative, resolve as resolveDir } from 'node:path';
 import { defineConfig, type Options } from 'tsup';
 
-export const createTsupConfig = ({
-	globalName = undefined,
-	format = ['esm', 'cjs', 'iife'],
-	target = 'es2021',
-	sourcemap = true
-}: ConfigOptions = {}) =>
+export const createTsupConfig = (options: Options = {}) =>
 	defineConfig({
 		clean: true,
 		dts: true,
 		entry: ['src/index.ts'],
-		format,
+		format: ['esm', 'cjs', 'iife'],
 		minify: false,
 		skipNodeModulesBundle: true,
-		sourcemap,
-		target,
+		sourcemap: true,
+		target: 'esnext',
 		tsconfig: relative(__dirname, resolveDir(process.cwd(), 'src', 'tsconfig.json')),
 		keepNames: true,
-		globalName
+		...options
 	});
-
-type ConfigOptions = Pick<Options, 'sourcemap' | 'target' | 'format' | 'globalName'>;
