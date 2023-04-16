c74181b9928a35a5280306bcebe57ad0ec22ce57
parbez <imranbarbhuiya.fsd@gmail.com>
2023-04-16 05:31:51 +0530

    chore: bump plugin markdown with patch

--- packages/tagscript-plugin-discord/src/lib/Transformer/Base.ts
+++ packages/tagscript-plugin-discord/src/lib/Transformer/Base.ts
@@ -14,7 +14,7 @@ export interface SafeValues<T> {
 }
 
 /**
- * Transformer for {@link discord.js} objects.
+ * Transformer for {@link discord.js!} objects.
  *
  * @typeParam T - The base type.
  */
